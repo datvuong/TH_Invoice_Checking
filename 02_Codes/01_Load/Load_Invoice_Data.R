278af0121292b6b1f7a77d41cd36a5814c54f64a
datvuong <dat.vuong88@gmail.com>
2016-02-29 20:35:49 +0700

    Kerry_updated_20150229

--- 02_Codes/01_Load/Load_Invoice_Data.R
+++ 02_Codes/01_Load/Load_Invoice_Data.R
@@ -31,19 +31,19 @@ LoadInvoiceData <- function(invoicePath) {
                   "destination_branch", "delivery_zone_zip_code", "rate_type")
     fullData <- NULL
     for (ifile in excelFiles) {
-      if (file_ext(ifile) %in% c("xls", "xlsx")) {
-        wb <- loadWorkbook(file.path(invoicePath, ifile))
-        invoiceFileData <- readWorksheet(wb, 1, colTypes = c(XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
-                                                             XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
-                                                             XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
-                                                             XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
-                                                             XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
-                                                             XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
-                                                             XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
-                                                             XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.STRING,
-                                                             XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING))
-        names(invoiceFileData) <- colNames
-      } else {
+#       if (file_ext(ifile) %in% c("xls", "xlsx")) {
+#         wb <- loadWorkbook(file.path(invoicePath, ifile))
+#         invoiceFileData <- readWorksheet(wb, 1, colTypes = c(XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
+#                                                              XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
+#                                                              XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING,
+#                                                              XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
+#                                                              XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
+#                                                              XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
+#                                                              XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC,
+#                                                              XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.NUMERIC, XLC$DATA_TYPE.STRING,
+#                                                              XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING, XLC$DATA_TYPE.STRING))
+#         names(invoiceFileData) <- colNames
+#       } else {
         invoiceFileData <- read_csv(file.path(invoicePath, ifile), skip = 1,
                                     col_names = colNames,
                                     col_types = cols(col_character(), col_character(), col_character(),
@@ -55,10 +55,11 @@ LoadInvoiceData <- function(invoicePath) {
                                                   col_double(), col_double(), col_double(),
                                                   col_double(), col_double(), col_character(),
                                                   col_character(), col_character(), col_character()))
-      }
+      # }
       
       invoiceFileData %<>%
         mutate(package_chargeable_weight = package_weight) %>%
+        mutate(tracking_number = ifelse(substr(tracking_number, 1, 1) == "1", tracking_number_rts, tracking_number)) %>%
         mutate(tracking_number = toupper(tracking_number)) %>%
         mutate(rawFile = ifile)
       
